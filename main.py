8bed8ed2d187d4bc926bc33003ce9c27f711f697
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-26 18:04:12 -0300

    chore: change try catch structe

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import os
 mergedCommit = input("Enter the commit you want to revert: ")
 mergedCommit = str(mergedCommit)
 
+
+print("Getting commits after merge")
+commits = [os.system(f"git rev-list {mergedCommit}...HEAD --reverse")]
+print(f"Commit hashs: {commits}\n")
+
+print("\nGetting commit before merge")
+previeousCommit = os.system(f"git rev-parse --short {mergedCommit}^1")
+print(f"Commit hashs: {previeousCommit}\n")
+
+print("\nExecuting reset --hard")
+os.system(f"git reset --hard {previeousCommit}")
+print("\nExecuting cherry pick")
+
 try:
-    print("Getting commits after merge")
-    commits = [os.system(f"git rev-list {mergedCommit}...HEAD --reverse")]
-    print(f"Commit hashs: {commits}\n")
-
-    print("\nGetting commit before merge")
-    previeousCommit = os.system(f"git rev-parse --short {mergedCommit}^1")
-    print(f"Commit hashs: {previeousCommit}\n")
-
-    print("\nExecuting reset --hard")
-    os.system(f"git reset --hard {previeousCommit}")
-    print("\nExecuting cherry pick")
-
     for commit in commits:
         print(f"Executing cherry pick of {commit}")
         os.system(f"cherry-pick {commit} --strategy-option=theirs")
